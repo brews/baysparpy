ae3d8705c05097d884bf43c9098ba5cd86a6ba63
Brewster Malevich <sbmalev@gmail.com>
2018-01-02 12:31:37 -0700

    Initial skeleton functions

---
+++ bayspar/__init__.py
@@ -0,0 +1,1 @@
+from predict import predict_tex, predict_subt, predict_sst
---
+++ bayspar/predict.py
@@ -0,0 +1,10 @@
+def predict_tex():
+    pass
+
+
+def predict_sst(analog=None):
+    pass
+
+
+def predict_subt(analog=None):
+    pass
