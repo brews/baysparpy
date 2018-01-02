092b4a2cbbfb8848e149d2f7e6a13be841939179
Brewster Malevich <sbmalev@gmail.com>
2018-01-02 16:56:07 -0700

    Add location methods for posterior draws.

--- bayspar/posterior/core.py
+++ bayspar/posterior/core.py
@@ -9,22 +9,51 @@ SST_PATH = os.path.join(HERE, 'Output_SpatAg_subT')
 
 
 def get_draws(flstr, drawtype):
-    """Return single squeezed array from MATLAB draw file"""
+    """Grab single squeezed array from MATLAB draw file
+    """
     flpath = None
     if drawtype.lower() == 'sst':
         flpath = os.path.join(SST_PATH, flstr)
     elif drawtype.lower() == 'subt':
         flpath = os.path.join(SUBT_PATH, flstr)
-    var_str = os.path.splitext(flstr)[0]
-    return loadmat(flpath, squeeze_me=True)[var_str]
+    variable = os.path.splitext(flstr)[0]
+    return loadmat(flpath, squeeze_me=True)[variable]
 
 
 class Draws:
+    """Spatially-aware posterior draws
+    """
     def __init__(self, alpha_samples_comp, beta_samples_comp, tau2_samples, locs_comp):
+        self._half_grid_space = 10
         self.alpha_samples_comp = np.array(alpha_samples_comp)
         self.beta_samples_comp = np.array(beta_samples_comp)
         self.tau2_samples = np.array(tau2_samples)
         self.locs_comp = np.array(locs_comp)
+
+    def _index_near(self, lat, lon):
+        """Get gridpoint index nearest a lat lon
+        """
+        assert -90 <= lat <= 90
+        assert -180 < lon <= 180
+        lon_adiff = np.abs(self.locs_comp[:, 0] - lon) <= self._half_grid_space
+        lat_adiff = np.abs(self.locs_comp[:, 1] - lat) <= self._half_grid_space
+        return np.where(lon_adiff & lat_adiff)
+
+    def find_nearest_latlon(self, lat, lon):
+        """Find draws gridpoint nearest a given lat lon
+        """
+        idx = self._index_near(lat, lon)
+        # Squeeze and flip so returns as latlon, not lonlat.
+        latlon = self.locs_comp[idx].squeeze()[::-1]
+        return latlon
+
+    def find_alphabeta_near(self, lat, lon):
+        """Find alpha and beta samples nearest a given location
+        """
+        idx = self._index_near(lat, lon)
+        alpha_select = self.alpha_samples_comp[idx].squeeze()
+        beta_select = self.beta_samples_comp[idx].squeeze()
+        return alpha_select, beta_select
 
 
 sst_draws = Draws(alpha_samples_comp=get_draws('alpha_samples_comp.mat', 'sst'),
--- bayspar/predict.py
+++ bayspar/predict.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+from bayspar.posterior import sst_draws, subt_draws
+
+
 def predict_tex():
     pass
 
@@ -8,3 +13,9 @@ def predict_sst(analog=None):
 
 def predict_subt(analog=None):
     pass
+
+
+def predict_seatemp(dats, lon, lat, prior_std, temptype, nens=5000):
+    assert temptype in ['sst', 'subt']
+    assert -180 <= lon <= 180
+    assert -90 <= lat <= 90
