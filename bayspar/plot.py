6542e9a8b473ec9e93b9da930ec161cd915ee0bd
Brewster Malevich <sbmalev@gmail.com>
2018-01-18 09:44:17 -0700

    Complete basic analogmap function.

--- bayspar/plot.py
+++ bayspar/plot.py
@@ -14,7 +14,7 @@ def map_texobs(ax=None, texobs=None, obstype=None):
         ax.coastlines()
 
     ax.scatter(texobs.obslocs[:, 0], texobs.obslocs[:, 1], marker='.',
-               transform=ccrs.Geodetic(), label=r'$TEX_{86} obs')
+               transform=ccrs.Geodetic(), label=r'$TEX_{86} obs', zorder=3)
     return ax
 
 
@@ -31,7 +31,7 @@ def map_site(prediction, latlon=None, ax=None):
         ax.coastlines()
 
     ax.plot(latlon[1], latlon[0], marker='^', transform=ccrs.Geodetic(),
-            label='Prediction')
+            label='Prediction', color='C1', zorder=4)
     return ax
 
 
@@ -44,7 +44,7 @@ def map_analog_boxes(prediction, ax=None):
 
     ys, xs = get_grid_corners(prediction.analog_gridpoints)
     for y, x in zip(ys, xs):
-        ax.plot(x, y, transform=ccrs.PlateCarree())
+        ax.plot(x, y, transform=ccrs.PlateCarree(), color='black')
 
     return ax
 
@@ -102,5 +102,18 @@ def predictplot(prediction, ylabel=None, x=None, xlabel=None, spaghetti=False, a
     return ax
 
 
-def analogmap():
-    raise NotImplementedError
+def analogmap(prediction, latlon=None, ax=None):
+    """Map analog prediction with grids used for analog and TEX86 sites
+    """
+    if ax is None:
+        ax = plt.gca(projection=ccrs.Robinson(central_longitude=0))
+        ax.coastlines(color='#7f7f7f')
+
+    ax = map_texobs(obstype=prediction.temptype, ax=ax)
+
+    if latlon is not None:
+        ax = map_site(prediction, latlon=latlon, ax=ax)
+
+    ax = map_analog_boxes(prediction, ax=None)
+
+    return ax
