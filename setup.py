50c3be1942bfdad0ea5334381d0c4b74860eb65e
Brewster Malevich <sbmalev@gmail.com>
2018-03-07 21:03:29 -0700

    Release v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='baysparpy',
-    version='0.0.1',
+    version='0.0.2',
     description='A Python implementation of BAYSPLINE',
     license='GPLv3',
 
